6c14a3b832b3b0c77daff926ea7ef5562430fb6a
home <home@DESKTOP-F2BTQ2A>
2023-05-07 18:43:07 +0900

    초기 커밋.

---
+++ ExcelSort/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ExcelSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SearchBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            this.button1.Location = new System.Drawing.Point(694, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "파일 불러오기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeight = 25;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.GridColor = System.Drawing.Color.Black;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 87);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(794, 361);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            this.textBox1.Location = new System.Drawing.Point(492, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += textBox1_KeyDown;
+            // 
+            // SearchBtn
+            // 
+            this.SearchBtn.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.SearchBtn.Location = new System.Drawing.Point(694, 57);
+            this.SearchBtn.Name = "SearchBtn";
+            this.SearchBtn.Size = new System.Drawing.Size(94, 23);
+            this.SearchBtn.TabIndex = 3;
+            this.SearchBtn.Text = "Search";
+            this.SearchBtn.UseVisualStyleBackColor = true;
+            this.SearchBtn.Click += new System.EventHandler(this.SearchBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SearchBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SearchBtn;
+    }
+}
+
---
+++ ExcelSort/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Data.OleDb;
+using System.IO;
+using ExcelDataReader;
+using System.Linq.Expressions;
+using System.Data.Common;
+using System.Text.RegularExpressions;
+
+namespace ExcelSort
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.InitialDirectory = "C:\\";
+                openFileDialog.Filter = "Excel files (*.xlsx, *,xls)|*.xlsx;*.xls|All files (*.*)|*.*";
+                openFileDialog.Multiselect = false;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    ImportExcelData_Read(openFileDialog.FileName, dataGridView1);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void ImportExcelData_Read(string fileName, DataGridView readdata)
+        {
+            // 엑셀 문서 내용 추출
+            string connectionString = string.Empty;
+
+            if (File.Exists(fileName))
+            {
+                if (Path.GetExtension(fileName).ToLower() == ".xls")
+                {   
+                    connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; Data Source={0};Extended Properties=Excel 8.0;", fileName);
+                }
+                else if (Path.GetExtension(fileName).ToLower() == ".xlsx")
+                {
+                    connectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0; Data Source={0};Extended Properties=Excel 12.0;", fileName);
+                }
+            }
+            DataSet data = new DataSet();
+
+            string strQuery = "SELECT * FROM [Sheet1$]";  // 엑셀 시트명 Sheet1의 모든 데이터를 가져오기
+            OleDbConnection oleConn = new OleDbConnection(connectionString);
+            oleConn.Open();
+
+            OleDbCommand oleCmd = new OleDbCommand(strQuery, oleConn);
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(oleCmd);
+
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            data.Tables.Add(dataTable);
+
+            readdata.DataSource = data.Tables[0].DefaultView;
+
+            for (int i = 0; i < readdata.Columns.Count; i++)
+            {
+                readdata.AutoResizeColumn(i, DataGridViewAutoSizeColumnMode.AllCells);
+            }
+            readdata.AllowUserToAddRows = false;
+            dataGridView1.DefaultCellStyle = readdata.DefaultCellStyle;
+
+            dataTable.Dispose();
+            dataAdapter.Dispose();
+            oleCmd.Dispose();
+
+            oleConn.Close();
+            oleConn.Dispose();
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            string searchText = textBox1.Text.Trim();
+            SearchRow(searchText);
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            string searchText = textBox1.Text.Trim();
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                SearchRow(searchText);
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void SearchRow(string searchText)
+        {
+            searchText = textBox1.Text.ToLower();
+            CurrencyManager currencyManager1 = (CurrencyManager)BindingContext[dataGridView1.DataSource];
+
+            try
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    bool isMatched = false;
+
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        if (dataGridView1.Rows[i].Cells[0].Value != null)
+                        {
+                            if (dataGridView1.Rows[i].Cells[0].Value.ToString().ToLower().Contains(searchText))
+                            {
+                                isMatched = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (isMatched)
+                    {
+                        dataGridView1.Rows[i].Visible = true;
+                    }
+                    else
+                    {
+                        currencyManager1.SuspendBinding();
+                        dataGridView1.Rows[i].Visible = false;
+                        currencyManager1.ResumeBinding();
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
